a142539b53b6982f9e58db58fd40afd30a00745a
satudatakemenagkonawe <maniuri391@gmail.com>
2026-03-07 08:10:00 +0800

    Update app_cat.py

--- app_cat.py
+++ app_cat.py
@@ -117,9 +117,9 @@ if not st.session_state.identitas_siap:
     st.markdown("<h1 style='text-align: center; color: #1a4d2e;'>🛡️ Sistem CAT Kemenag Konawe</h1>", unsafe_allow_html=True)
     with st.columns([1, 2, 1])[1]:
         with st.form("login_form"):
-            nama = st.text_input("Nama Lengkap")
+            nama = st.text_input("Nama Lengkap - HURUF BESAR TANPA GELAR")
             nip = st.text_input("Nomor Peserta")
-            if st.form_submit_button("Masuk ke Ruang Tes"):
+            if st.form_submit_button("Mulai Tes"):
                 if nama and nip and st.session_state.bank_soal:
                     st.session_state.nama, st.session_state.nip = nama, nip
                     st.session_state.identitas_siap = True
@@ -179,6 +179,7 @@ else:
             kirim_ke_sheets(st.session_state.nama, st.session_state.nip, st.session_state.theta, rel, sem, skor)
             st.session_state.sent = True
         st.info("Hasil telah dikirimkan secara otomatis ke Database Pusat Data Penilaian.")
+
 
 
 
