2a237c7393d92a677589cf46353e7e6875d99888
satudatakemenagkonawe <maniuri391@gmail.com>
2026-03-08 08:19:01 +0800

    Update app_cat.py

--- app_cat.py
+++ app_cat.py
@@ -1,8 +1,8 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import numpy as np
 import requests
-from streamlit_gsheets import GSheetsConnection
 import time
 import datetime
 
@@ -232,6 +232,7 @@ else:
             kirim_ke_sheets(st.session_state.nama, st.session_state.nip, st.session_state.theta, rel, sem, skor)
             st.session_state.sent = True
         st.info("Hasil telah dikirimkan secara otomatis ke Database Pusat Data Penilaian.")
+
 
 
 
